ad251e709b243133f729ba92b75d55cac6abc906
JordanTFA <Jordantfa@hotmail.co.uk>
2018-10-20 22:55:15 +0100

    Learned init and destroy methods

---
+++ HelloSpring/src/lifecycle/initdestroy/InitMain.java
@@ -0,0 +1,18 @@
+package lifecycle.initdestroy;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class InitMain {
+
+	public static void main(String[] args) {
+		
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		
+		HelloWorld obj = (HelloWorld)context.getBean("initDestroy");
+		obj.getMessage();
+		context.registerShutdownHook();
+
+	}
+
+}
